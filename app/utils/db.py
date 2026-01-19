429d59f08af2a6c1400dcdd60af94a1141965328
Mahek <mahekharivansh2008@gmail.com>
2026-01-19 22:32:43 +0530

    use Render DATABASE_URL

--- app/utils/db.py
+++ app/utils/db.py
@@ -1,13 +1,7 @@
-import psycopg2
 import os
+import psycopg2
 
 def get_db_connection():
-    conn = psycopg2.connect(
-        dbname=os.getenv("PGDATABASE") or os.getenv("DB_NAME"),
-        user=os.getenv("PGUSER") or os.getenv("DB_USER"),
-        password=os.getenv("PGPASSWORD") or os.getenv("DB_PASSWORD"),
-        host=os.getenv("PGHOST") or os.getenv("DB_HOST"),
-        port=os.getenv("PGPORT") or os.getenv("DB_PORT")
-    )
-    conn.autocommit = False
-    return conn        
+    database_url = os.getenv("DATABASE_URL")
+    conn = psycopg2.connect(database_url)
+    return conn
